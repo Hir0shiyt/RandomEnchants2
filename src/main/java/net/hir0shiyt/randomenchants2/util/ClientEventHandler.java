a8151589a0ccaf950bde113a8acf5f33fd95dbee
Hir0shiyt <hiroshicrackyt@gmail.com>
2024-04-02 18:33:54 +0200

    Probable NBT issue

--- src/main/java/net/hir0shiyt/randomenchants2/util/ClientEventHandler.java
+++ src/main/java/net/hir0shiyt/randomenchants2/util/ClientEventHandler.java
@@ -4,12 +4,10 @@ import net.hir0shiyt.randomenchants2.RandomEnchants2;
 import net.hir0shiyt.randomenchants2.config.ModConfig;
 import net.hir0shiyt.randomenchants2.enchantment.ModEnchantments;
 import net.minecraft.ChatFormatting;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.api.distmarker.Dist;
@@ -33,11 +31,6 @@ public class ClientEventHandler {
             if (event.getItemStack() != stack) return;
             tooltip.add(new TextComponent("Mining Bonus: " + ChatFormatting.GREEN + "+" + stack.getDamageValue() * .02));
             tooltip.add(new TextComponent("Damage Penalty: " + ChatFormatting.RED + "-" + stack.getDamageValue() * .02));
-            if (event.getItemStack().getItem() instanceof EnchantedBookItem) {
-                ListTag nbtTagList = EnchantedBookItem.getEnchantments(event.getItemStack());
-                for (int i = 0; i < nbtTagList.size(); ++i) {
-                }
-            }
         }
 
         else if ((EnchantUtils.hasEnch(player, ModEnchantments.DIMENSIONAL_SHUFFLE))) {
